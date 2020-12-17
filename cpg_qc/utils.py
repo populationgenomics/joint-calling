1ea976f5f14ecd336bc9cccb760c5415ee721144
vladsaveliev-eviltwin <vladislav.sav-eviltwin@gmail.com>
2020-12-17 14:59:16 +1100

    Validate command line arguments

--- cpg_qc/utils.py
+++ cpg_qc/utils.py
@@ -4,11 +4,31 @@ import subprocess
 import sys
 import time
 from os.path import isdir, isfile, exists
+from typing import Any, Callable
+
+import click
 from google.cloud import storage
 import hashlib
 
 
-def file_exists(path: str):
+def get_validation_callback(ext: str = None, must_exist: bool = False) -> Callable:
+    def callback(ctx: click.Context, param: click.Option, value: Any):
+        if ext:
+            assert isinstance(value, str), value
+            value = value.rstrip('/')
+            if not value.endswith(f'.{ext}'):
+                raise click.BadParameter(
+                    f'The argument {param.name} is expected to have '
+                    f'an extension .{ext}, got: {value}')
+        if must_exist:
+            if not file_exists(value):
+                raise click.BadParameter(
+                    f'The value for {param.name} is expected to exist: {value}')
+        return value
+    return callback
+
+
+def file_exists(path: str) -> bool:
     if path.startswith('gs://'):
         bucket = path.replace('gs://', '').split('/')[0]
         path = path.replace('gs://', '').split('/', maxsplit=1)[1]
@@ -42,7 +62,7 @@ def gs_cache_file(fpath: str, local_tmp_dir: str = None) -> str:
     return local_fpath
 
 
-def safe_mkdir(dirpath, descriptive_name=''):
+def safe_mkdir(dirpath: str, descriptive_name: str = '') -> str:
     """ Multiprocessing-safely and recursively creates a directory
     """
     if not dirpath:
