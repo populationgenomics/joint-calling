e012dce3d52078e3aed020b2531d51560b0564a5
vladsaveliev <vladislav.sav@gmail.com>
2021-01-03 21:26:13 +1100

    Update test scripts

--- test/test_combine_gvcfs.sh
+++ test/test_combine_gvcfs.sh
@@ -3,7 +3,13 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 combine_gvcfs \
-  --out-mt        ${DIR}/test_run/genomes.exomesubset.mt \
-  --sample-map    samples.toy.csv \
+  --out-mt        ${DIR}/test_run/genomes.mt \
+  --sample-map    ${DIR}/data/samples.toy.local.csv \
   --bucket        ${DIR}/test_run/combine_gvcfs/bucket \
   --local-tmp-dir ${DIR}/test_run/combine_gvcfs/local
+
+if [ $? -eq 0 ]; then
+  test -e ${DIR}/test_run/genomes.mt
+  test -e ${DIR}/test_run/genomes.metadata.ht
+fi
+
--- test/test_sample_qc.sh
+++ test/test_sample_qc.sh
@@ -3,10 +3,15 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 sample_qc \
-  --mt            ${DIR}/test_run/genomes.exomesubset.mt \
-  --meta-ht       ${DIR}/test_run/sample_qc/meta.ht \
-  --sample-map    samples.toy.csv \
-  --bucket        ${DIR}/test_run/sample_qc/bucket \
+  --mt            ${DIR}/test_run/genomes.mt \
+  --out-ht        ${DIR}/test_run/sample_qc.ht \
+  --sample-map    ${DIR}/data/samples.toy.local.csv \
+  --bucket        gs://playground-au/tmp/sample_qc/bucket \
   --local-tmp-dir ${DIR}/test_run/sample_qc/local
 
-cut -f2 test_run/sample_qc/meta.tsv | head -n2 | tail -n1 | grep -q true || exit 1
+if [ $? -eq 0 ]; then
+  test -e ${DIR}/test_run/sample_qc.ht
+  test -e ${DIR}/test_run/sample_qc.tsv
+  cut -f2 ${DIR}/test_run/sample_qc.tsv \
+    | head -n2 | tail -n1 | grep -q true || exit 1
+fi
