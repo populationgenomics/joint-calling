cc02d4030ea817e1d02794ee2a124f5a9f5ae74f
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-03-17 16:49:01 +1100

    Update test_copy to test invalid input

--- test/test_upload_processor.py
+++ test/test_upload_processor.py
@@ -2,11 +2,12 @@
 
 import unittest
 from unittest.mock import call, patch
+from google.api_core import exceptions
 from joint_calling.upload_processor import copy_files, delete_files
 
 
 class TestUploadProcessor(unittest.TestCase):
-    """A series of test cases for the upload processor"""
+    """Test cases for the upload processor"""
 
     def setUp(self):
         """ Initialises standard variables for testing """
@@ -15,17 +16,29 @@ class TestUploadProcessor(unittest.TestCase):
         self.standard_list = ['Sample1.gVCF']
         self.invalid_bucket = 'invalid-bucket'
 
-    @patch('joint_calling.upload_processor.storage')
-    def test_copy(self, mock_storage):
-        """Standard test case for the copy function, with standard input
-        No errors should be produced nor exceptions raised. Passes in a standard
-        list and assumes that the upload and main bucket are valid inputs"""
+    def test_copy(self):
+        """A series of test cases for the copy function, using mock objects
+        storage objects"""
 
-        copy_files(self.standard_list, self.upload_bucket, self.main_bucket)
-        mock_storage.Client.assert_called_once()
-        mock_client = mock_storage.Client()
-        calls = [call.get_bucket(self.upload_bucket), call.get_bucket(self.main_bucket)]
-        mock_client.assert_has_calls(calls, any_order=True)
+        # Standard test case. Input list and buckets valid. Tests the get_buckets is
+        # called twice, once for each input bucket.
+        with patch('joint_calling.upload_processor.storage') as mock_storage:
+            copy_files(self.standard_list, self.upload_bucket, self.main_bucket)
+            mock_storage.Client.assert_called_once()
+            mock_client = mock_storage.Client()
+            calls = [
+                call.get_bucket(self.upload_bucket),
+                call.get_bucket(self.main_bucket),
+            ]
+            mock_client.assert_has_calls(calls, any_order=True)
+
+        # Invalid bucket test case. Tests that an exception is raised when an invalid
+        # bucket name is provided.
+        with patch('joint_calling.upload_processor.storage') as mock_storage_invalid:
+            mock_bucket = mock_storage_invalid.Client()
+            mock_bucket.get_bucket.side_effect = exceptions.NotFound('Bucket Not Found')
+            with self.assertRaises(Exception):
+                copy_files(self.standard_list, self.upload_bucket, self.main_bucket)
 
     @patch('joint_calling.upload_processor.storage')
     def test_delete(self, mock_storage):
