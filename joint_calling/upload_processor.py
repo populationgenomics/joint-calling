971c6e56df0019a5daf60bf09825cc692af84d7c
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-03-18 20:40:18 +1100

    Create move_files function * Handles several cases including previous failures or outdated copies

--- joint_calling/upload_processor.py
+++ joint_calling/upload_processor.py
@@ -10,6 +10,52 @@ from google.cloud import storage
 from google.api_core.exceptions import NotFound
 
 
+def move_files(
+    sample_files: List[str], source_bucket_name: str, destination_bucket_name: str
+):
+    """Given a list of file names, a source bucket and
+    a destination bucket, this function will move the identified files
+    to their new location. The function assumes that the file names have
+    been validated at an earlier stage. The function will raise an error if invalid
+    bucket details, or invalid sample names are produced."""
+
+    # Connecting to buckets
+    try:
+        storage_client = storage.Client()
+        source_bucket = storage_client.get_bucket(source_bucket_name)
+        destination_bucket = storage_client.get_bucket(destination_bucket_name)
+    except NotFound as invalid_details:
+        raise NotFound('Invalid bucket details') from invalid_details
+
+    # Iterating through files to be moved
+    for sample in sample_files:
+        sample_blob = source_bucket.get_blob(sample)
+        destination_blob = destination_bucket.get_blob(sample)
+        # Sample exists in both the source and the destination buckets.
+        if destination_blob and sample_blob:
+            # Check if the files have identical content
+            if destination_blob.crc32c == sample_blob.crc32c:
+                # skip the copy as the same file already exists in the
+                # destination folder
+                source_bucket.delete_blob(sample_blob.name)
+            else:
+                # A file with the same name exists, however it contains different
+                # content, so the new version is copied across
+                source_bucket.copy_blob(sample_blob, destination_bucket)
+                source_bucket.delete_blob(sample_blob.name)
+        # Sample file has not been added to the destination previously
+        elif sample_blob and not destination_blob:
+            # Move the file
+            source_bucket.copy_blob(sample_blob, destination_bucket)
+            source_bucket.delete_blob(sample_blob.name)
+        elif destination_blob and not sample_blob:
+            # The sample was moved to the destination in a previous execution
+            # of the pipeline.
+            continue
+        else:
+            raise NotFound(f'{sample} was not found')
+
+
 def copy_files(
     sample_files: List[str], source_bucket_name: str, destination_bucket_name: str
 ) -> Tuple[List[str], List[str]]:
