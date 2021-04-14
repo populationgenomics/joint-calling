528ad5183e6fec0d77eee7f292698faa1bfc1ae5
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-04-14 17:02:53 +1000

    Bump version: 0.1.19 → 0.1.20

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='joint-calling',
-    version='0.1.19',
+    version='0.1.20',
     description='Pipeline for joint calling, sample and variant QC for WGS germline '
     'variant calling data',
     long_description=open('README.md').read(),
