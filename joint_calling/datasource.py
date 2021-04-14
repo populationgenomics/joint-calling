135a0a69a8f194612bd7d26da4479aa7240ad2e3
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-04-14 15:43:10 +1000

    Add TableColumn class and get_formula function * get_formula function created to allow for multiple filters as input

--- joint_calling/datasource.py
+++ joint_calling/datasource.py
@@ -5,18 +5,22 @@ processor"""
 
 import json
 import abc
-from typing import List
+from typing import List, Dict, Any
+from enum import Enum
 from airtable import Airtable
 from google.cloud import secretmanager
 
 
+class TableColumn(Enum):
+    """Defines a series of table columns, and their corresponding ID's
+    to be used for filtering"""
+
+    STATUS = 'Status'
+    SAMPLE_ID = 'Sample_ID'
+
+
 class Datasource(metaclass=abc.ABCMeta):
     """A source of data"""
-
-    @abc.abstractmethod
-    def get_table(self):
-        """Obtain a single table object from the
-        data source"""
 
     @abc.abstractmethod
     def get_file_list(self, col, val) -> List[str]:
@@ -41,7 +45,7 @@ class AirTableDatasource:
         config = json.loads(config_str)
         self.project_config = config.get(project)
 
-    def get_table(self) -> Airtable:
+    def _get_table(self) -> Airtable:
         """Authenticates to Airtable and returns the table specified
         in the config file."""
 
@@ -55,14 +59,60 @@ class AirTableDatasource:
 
         return table
 
-    def get_file_list(self, col: str, val: str, extension: str) -> List[str]:
-        """Returns a list of file names from the database based on a filter
-        condition."""
-        table = self.get_table()
+    def get_file_list(
+        self, filters: Dict[TableColumn, Any], extension: str
+    ) -> List[str]:
+        """Returns a list of file names from the database based on filter
+        conditions.
+
+        Parameters
+        ==========
+        filters: Dict[TableColumn, any]
+        A dictionary where the key is a specific table column
+        and the value represents the specific condition that needs to be matched.
+        For example:
+        {TableColumn.status: "Done"}
+        This represents a filter on the status column, where status is set to
+        Done.
+
+        extension: str
+        The file extension for the set of files to be returned.
+        All files will be listed with this specific file extension.
+        Assumes that this extension is valid & consistent for all samples.
+        For example: '.gVCF', '.txt', etc.
+
+        Returns
+        =======
+
+
+        """
+        table = self._get_table()
+        formula = get_formula(filters)
         sample_files = []
-        for page in table.get_iter(formula=f"{col}='{val}'"):
+        for page in table.get_iter(formula=formula):
             for record in page:
                 sample = record['fields']['Sample_ID']
                 sample_files.append(f'{sample}.{extension}')
 
         return sample_files
+
+
+def get_formula(filters: Dict[TableColumn, Any]) -> str:
+    """Creates a forumula with the appropriate syntax for the datasource.
+    Given a dictionary of filters, the function returns a formatted string
+    in accordance with the guidelines of the datasource.
+    """
+
+    # In accordance with AirTable formula syntax
+    formula = 'AND('
+
+    # Append each filter and property to the formula
+    for col_filter in filters:
+        print(type(col_filter))
+        formula = formula + col_filter.value + "='" + filters[col_filter] + "', "
+
+    # Format & clean up
+    formula = formula.strip(', ')
+    formula = formula + ')'
+
+    return formula
