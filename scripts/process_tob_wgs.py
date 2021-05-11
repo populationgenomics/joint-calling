d0505070f0b9f8b9737d66cabbfe897137318276
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-05-11 16:14:58 +1000

    Fix archive path and file read

--- scripts/process_tob_wgs.py
+++ scripts/process_tob_wgs.py
@@ -4,21 +4,24 @@ Process TOB-WGS files within upload bucket
 """
 
 import os
-from typing import List
 import hailtop.batch as hb
 from joint_calling.upload_processor import batch_move_files
 
 
-def filter_files(files: List[str], prefix: str):
+def filter_files():
     """ Determine files that should be moved to main vs archive bucket """
     files_for_main = []
     files_for_archive = []
-    for file_path in files:
-        file_path = file_path.strip()
-        if file_path.endswith(('.g.vcf.gz', '.g.vcf.gz.tbi', '.g.vcf.gz.md5', 'csv')):
-            files_for_main.append(file_path[len(prefix) :])
-        else:
-            files_for_archive.append(file_path[len(prefix) :])
+    prefix = 'gs://cpg-tob-wgs-upload/'
+    with open('../test/data/tob_wgs_batch2.txt') as f:
+        for file_path in f:
+            file_path = file_path.strip()
+            if file_path.endswith(
+                ('.g.vcf.gz', '.g.vcf.gz.tbi', '.g.vcf.gz.md5', 'csv')
+            ):
+                files_for_main.append(file_path[len(prefix) :])
+            else:
+                files_for_archive.append(file_path[len(prefix) :])
 
     return files_for_main, files_for_archive
 
@@ -26,10 +29,7 @@ def filter_files(files: List[str], prefix: str):
 if __name__ == '__main__':
 
     # Process input file of sample names
-    file_list = open('../test/data/tob_wgs_batch2.txt')
-    lines = file_list.readlines()
-    PREF = 'gs://cpg-tob-wgs-upload/'
-    main_files, archive_files = filter_files(lines, PREF)
+    main_files, archive_files = filter_files()
 
     # Setting up inputs for batch_move_files
     upload_prefix = os.path.join('cpg-tob-wgs-upload')
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     )
 
     main_batch.run()
-    archive_prefix = os.path.join('vivian-dev-archive', 'cram', 'batch2')
+    archive_prefix = os.path.join('cpg-tob-archive', 'cram', 'batch2')
 
     # Moving the files to the archive bucket
     archive_batch = hb.Batch(name='Move Files to Archive')
