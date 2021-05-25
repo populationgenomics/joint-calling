f36cde1e04e54636afa15aeadf0ed9726029d40d
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-05-25 16:14:28 +1000

    Add csv job as dependency, remove sep batch

--- scripts/process_uploads.py
+++ scripts/process_uploads.py
@@ -106,7 +106,7 @@ def run_processor(batch_number):
     batch = hb.Batch(name='Process files', backend=service_backend)
 
     # Moving the files to the main bucket
-    batch_move_files(
+    main_jobs = batch_move_files(
         batch,
         main_files,
         upload_prefix,
@@ -121,7 +121,7 @@ def run_processor(batch_number):
     # archive_prefix = os.path.join(f'cpg-{project}-archive', 'cram', batch_number)
 
     # Moving the files to the archive bucket
-    batch_move_files(
+    archive_jobs = batch_move_files(
         batch,
         archive_files,
         upload_prefix,
@@ -130,27 +130,13 @@ def run_processor(batch_number):
         key,
     )
 
-    status = batch.run().status()
+    csv_job = batch.new_job(name=f'Move {basename(csv_path)}')
+    csv_job.depends(main_jobs + archive_jobs)
+    final_csv_location = join('gs://', main_prefix, basename(csv_path))
+    csv_job.command(f'gsutil mv {csv_path} {final_csv_location}')
 
-    if status['state'] == 'success':
-        # Once all the files have been successfully processed, move the csv file.
-        final_csv_location = join('gs://', main_prefix, basename(csv_path))
-        final_batch = hb.Batch(name='Move csv', backend=service_backend)
-        j = final_batch.new_job(name=f'Move {basename(csv_path)}')
-        j.image(docker_image)
-        j.command(
-            'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
-        )
-        j.command(f'gsutil mv {csv_path} {final_csv_location}')
-        final_batch.run()
-
-    else:
-        batch_url = (
-            f"https://batch.hail.populationgenomics.org.au/batches/{status['id']}"
-        )
-        message = f'The batch was not successful. See {batch_url}for more information'
-        raise Exception(message)
+    batch.run()
 
 
 if __name__ == '__main__':
-    run_processor('batch5')
+    run_processor('batch6')
