15841604d314561bfd4e1d4716ec9e5ada83228a
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-05-28 13:46:51 +1000

    Handle CSV file * Considers previous batch CSV so that only new files are moved

--- scripts/process_uploads.py
+++ scripts/process_uploads.py
@@ -15,35 +15,58 @@ import hailtop.batch as hb
 from joint_calling.upload_processor import batch_move_files
 
 
-def determine_samples(upload_bucket: str):
+def return_samples_from_csv(local_csv_path):
+    """ Determines list of samples from a given csv file. """
+
+    samples: List[str] = []
+    with open(local_csv_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            samples.append(row['sample.sample_name'])
+
+    return samples
+
+
+def determine_samples(upload_bucket: str, previous_batch_path: str):
     """Determine files that should be moved to main vs archive bucket
     Parameters
     =========
-    input_bucket:  List[str] The bucket to be processed
+    upload_bucket : Name of the upload bucket
+    previous_batch_path : Previous batch folder
 
     Assumptions
     ==========
     Only one .csv file exists in the bucket.
     """
     local_tmp_dir = tempfile.mkdtemp()
-    samples: List[str] = []
+    all_samples: List[str] = []
+    previous_samples: List[str] = []
 
-    # Pull the CSV file that contains the list of samples in the current batch
-
+    # Pull the samples listed in the most recent CSV file
     cmd = f'gsutil ls \'gs://{upload_bucket}/*.csv\''
-    csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
-    local_csv_path = join(local_tmp_dir, basename(csv_file_path))
+    curr_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
+    local_curr_csv_path = join(local_tmp_dir, basename(curr_csv_file_path))
     subprocess.run(
-        f'gsutil cp {csv_file_path} {local_csv_path}', check=False, shell=True
+        f'gsutil cp {curr_csv_file_path} {local_curr_csv_path}', check=False, shell=True
     )
 
-    with open(local_csv_path) as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            samples.append(row['sample.sample_name'])
+    all_samples = return_samples_from_csv(local_curr_csv_path)
+
+    # Pull the samples listed in the CSV file from the previous batch
+    cmd = f'gsutil ls \'gs://{previous_batch_path}/*.csv\''
+    prev_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
+    local_prev_csv_path = join(local_tmp_dir, basename(prev_csv_file_path))
+    subprocess.run(
+        f'gsutil cp {prev_csv_file_path} {local_prev_csv_path}', check=False, shell=True
+    )
+
+    previous_samples = return_samples_from_csv(local_prev_csv_path)
+
+    samples = set(all_samples) - set(previous_samples)
 
     shutil.rmtree(local_tmp_dir)
-    return samples, csv_file_path
+
+    return samples, curr_csv_file_path
 
 
 def generate_file_list(samples: List[str]):
@@ -57,7 +80,7 @@ def generate_file_list(samples: List[str]):
     return main_files, archive_files
 
 
-def run_processor(batch_number: str):
+def run_processor(batch_number: str, prev_batch: str):
     """Set up and execute batch_move_files
 
     Parameters
@@ -70,14 +93,14 @@ def run_processor(batch_number: str):
 
     # Setting up inputs for batch_move_files
     project = os.getenv('HAIL_BILLING_PROJECT')
-
     upload_prefix = os.path.join(f'cpg-{project}-upload')
     main_prefix = os.path.join(f'cpg-{project}-main', 'gvcf', batch_number)
+    prev_prefix = os.path.join(f'cpg-{project}-main', 'gvcf', prev_batch)
     docker_image = os.environ.get('DRIVER_IMAGE')
     key = os.environ.get('GSA_KEY')
 
-    # Process input file of sample names
-    samples, csv_path = determine_samples(upload_prefix)
+    # Determine files to be processed
+    samples, csv_path = determine_samples(upload_prefix, prev_prefix)
     main_files, archive_files = generate_file_list(samples)
 
     # Initialize the service backend.
@@ -112,6 +135,7 @@ def run_processor(batch_number: str):
         key,
     )
 
+    # Move the csv file, after main and archive buckets have been processed.
     csv_job = batch.new_job(name=f'Move {basename(csv_path)}')
     csv_job.image(docker_image)
     csv_job.command(
@@ -128,4 +152,4 @@ def run_processor(batch_number: str):
 if __name__ == '__main__':
 
     # Run the processor on the 4th batch, i.e. batch3.
-    run_processor('batch3')
+    run_processor('batch3', 'batch2')
