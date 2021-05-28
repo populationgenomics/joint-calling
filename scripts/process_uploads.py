d858bdc71a16506d4bed04217cb6fb4c32507e40
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-05-28 16:44:51 +1000

    WIP Use GCS Client Library

--- scripts/process_uploads.py
+++ scripts/process_uploads.py
@@ -4,30 +4,40 @@ Processes files uploaded to gcp
 """
 
 import os
+import io
 import csv
 from os.path import join, basename
 import subprocess
-import tempfile
-import shutil
 from typing import List
 import hail as hl
 import hailtop.batch as hb
+from google.cloud import storage
 from joint_calling.upload_processor import batch_move_files
 
 
-def samples_from_csv(local_csv_path):
+def samples_from_csv(bucket, path):
     """ Determines list of samples from a given csv file. """
 
+    client = storage.Client()
+    bucket = client.get_bucket({bucket})
+    full_path = os.path.join('gs://', bucket, path)
+
+    cmd = f'gsutil ls \'{full_path}\''
+    csv_path = subprocess.check_output(cmd, shell=True).decode().strip()
+    blob = bucket.get_blob(basename(csv_path))
+    data = blob.download_as_string().decode('utf-8')
+    csv_reader = csv.DictReader(io.StringIO(data))
+
     samples: List[str] = []
-    with open(local_csv_path) as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            samples.append(row['sample.sample_name'])
+    for row in csv_reader:
+        samples.append(row['sample.sample_name'])
 
     return samples
 
 
-def determine_samples(upload_bucket: str, previous_batch_path: str):
+def determine_samples(
+    upload_bucket: str, upload_path: str, main_bucket: str, previous_batch_path: str
+):
     """Determine files that should be moved to main vs archive bucket.
     Determines the difference between the latest CSV file within upload
     and the CSV file from the most recent batch as the list of samples to
@@ -37,33 +47,44 @@ def determine_samples(upload_bucket: str, previous_batch_path: str):
     ==========
     Only one .csv file exists in the upload bucket and previous batch directory.
     """
-    local_tmp_dir = tempfile.mkdtemp()
+    # local_tmp_dir = tempfile.mkdtemp()
     all_samples: List[str] = []
     previous_samples: List[str] = []
 
-    # Pull the samples listed in the most recent CSV file
-    cmd = f'gsutil ls \'gs://{upload_bucket}/*.csv\''
-    curr_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
-    local_curr_csv_path = join(local_tmp_dir, basename(curr_csv_file_path))
-    subprocess.run(
-        f'gsutil cp {curr_csv_file_path} {local_curr_csv_path}', check=False, shell=True
-    )
+    # cmd = f'gsutil ls \'gs://{upload_bucket}/*.csv\''
+    # curr_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
+    # blob = bucket.get_blob(basename(curr_csv_file_path))
+    # data = blob.download_as_string().decode("utf-8")
+    # reader_list = csv.DictReader(io.StringIO(data))
+    upload_path = os.path.join('gs://', upload_bucket, upload_path)
 
-    all_samples = samples_from_csv(local_curr_csv_path)
+    # Pull the samples listed in the most recent CSV file
+    cmd = f'gsutil ls \'{upload_path}\''
+    curr_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
+    # local_curr_csv_path = join(local_tmp_dir, basename(curr_csv_file_path))
+    # subprocess.run(
+    #     f'gsutil cp {curr_csv_file_path} {local_curr_csv_path}', check=False, shell=True
+    # )
+
+    all_samples = samples_from_csv(upload_bucket, '')
 
     # Pull the samples listed in the CSV file from the previous batch
-    cmd = f'gsutil ls \'gs://{previous_batch_path}/*.csv\''
-    prev_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
-    local_prev_csv_path = join(local_tmp_dir, basename(prev_csv_file_path))
-    subprocess.run(
-        f'gsutil cp {prev_csv_file_path} {local_prev_csv_path}', check=False, shell=True
-    )
+    # cmd = f'gsutil ls \'gs://{previous_batch_path}/*.csv\''
+    # prev_csv_file_path = subprocess.check_output(cmd, shell=True).decode().strip()
+    # blob = bucket.get_blob(basename(curr_csv_file_path))
+    # data = blob.download_as_string().decode("utf-8")
+    # reader_list = csv.DictReader(io.StringIO(data))
 
-    previous_samples = samples_from_csv(local_prev_csv_path)
+    # local_prev_csv_path = join(local_tmp_dir, basename(prev_csv_file_path))
+    # subprocess.run(
+    #     f'gsutil cp {prev_csv_file_path} {local_prev_csv_path}', check=False, shell=True
+    # )
+
+    previous_samples = samples_from_csv(main_bucket, previous_batch_path)
 
     samples = set(all_samples) - set(previous_samples)
 
-    shutil.rmtree(local_tmp_dir)
+    # shutil.rmtree(local_tmp_dir)
 
     return samples, curr_csv_file_path
 
@@ -96,15 +117,29 @@ def run_processor(batch_number: str, prev_batch: str):
 
     # Setting up inputs for batch_move_files
     project = os.getenv('HAIL_BILLING_PROJECT')
-    upload_prefix = os.path.join(f'cpg-{project}-upload')
-    main_prefix = os.path.join(f'cpg-{project}-main', 'gvcf', batch_number)
-    prev_prefix = os.path.join(f'cpg-{project}-main', 'gvcf', prev_batch)
-    archive_prefix = os.path.join(f'cpg-{project}-archive', 'cram', batch_number)
+    # upload_bucket = os.path.join(f'cpg-{project}-upload')
+    # upload_prefix = os.path.join(upload_bucket)
+    # main_bucket = os.path.join(f'cpg-{project}-main')
+    # main_prefix = os.path.join(main_bucket, 'gvcf', batch_number)
+    # prev_prefix = os.path.join('gvcf', prev_batch)
+    # archive_prefix = os.path.join(f'cpg-{project}-archive', 'cram', batch_number)
+
+    upload_bucket = os.path.join(f'cpg-{project}-temporary')
+    upload_prefix = os.path.join(upload_bucket, 'vivian-test', 'upload')
+    main_bucket = os.path.join(f'cpg-{project}-temporary')
+    main_prefix = os.path.join(main_bucket, 'vivian-test', 'main', 'gvcf', batch_number)
+    prev_prefix = os.path.join('gvcf', prev_batch)
+    archive_prefix = os.path.join(
+        f'cpg-{project}-temporary', 'vivian-test', 'archive', 'cram', batch_number
+    )
+
     docker_image = os.environ.get('DRIVER_IMAGE')
     key = os.environ.get('GSA_KEY')
 
     # Determine files to be processed
-    samples, csv_path = determine_samples(upload_prefix, prev_prefix)
+    samples, csv_path = determine_samples(
+        upload_bucket, upload_prefix, main_bucket, prev_prefix
+    )
     main_files, archive_files = generate_file_list(samples)
 
     # Initialize the service backend.
