8769fa510b94298aeed86ee2b0cf6b38a5e7ae41
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-05-28 17:29:49 +1000

    WIP Fix Path

--- scripts/process_uploads.py
+++ scripts/process_uploads.py
@@ -24,7 +24,7 @@ def samples_from_csv(bucket_name, path):
 
     cmd = f'gsutil ls \'gs://{path}/*.csv\''
     csv_path = subprocess.check_output(cmd, shell=True).decode().strip()
-    blob = bucket.get_blob(basename(csv_path))
+    blob = bucket.get_blob(csv_path[len(bucket_name) :])
     data = blob.download_as_string().decode('utf-8')
     csv_reader = csv.DictReader(io.StringIO(data))
 
@@ -128,7 +128,7 @@ def run_processor(batch_number: str, prev_batch: str):
     upload_prefix = os.path.join(upload_bucket, 'vivian-test', 'upload')
     main_bucket = f'cpg-{project}-temporary'
     main_prefix = os.path.join(main_bucket, 'vivian-test', 'main', 'gvcf', batch_number)
-    prev_prefix = os.path.join('gvcf', prev_batch)
+    prev_prefix = os.path.join(main_prefix, 'vivian-test', 'main', 'gvcf', prev_batch)
     archive_prefix = os.path.join(
         f'cpg-{project}-temporary', 'vivian-test', 'archive', 'cram', batch_number
     )
