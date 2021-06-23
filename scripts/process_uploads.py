eb82d3656e2eec18aa26b4f60e08a3f6a2c14d69
Vivian Bakiris <79084890+vivbak@users.noreply.github.com>
2021-06-23 15:00:23 +1000

    Validate MD5  (#48)
    * Create further sink/gather structure
    * Updated generate_file_list to produce files divided according to sample, so each set of files related to a sample can be processed separately
    * Updated run_processor to iterate over each sample group
    * Created a skeleton structure for the validate_md5 job.
    
    * Add MD5 Validation Script
    
    * Catch unprovided docker image
    
    * Remove local testing artefacts
    
    * Fix variable names
    
    * Apply code review suggestions
    * Adding type hints, fixing comments, creating a SampleGroup NamedTuple, fixed unspotted path error
    
    * Apply suggestions from code review
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Fix SampleGroup Attribute
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/process_uploads.py
+++ scripts/process_uploads.py
@@ -10,15 +10,15 @@ most recently updated CSV and the CSV uploaded in a previous batch run.
 import os
 import io
 import csv
-from os.path import join, basename
-from typing import List
+from os.path import basename, join
+from typing import List, Optional, Set, Tuple
 import click
 import hailtop.batch as hb
 from google.cloud import storage
-from joint_calling.upload_processor import batch_move_files
+from joint_calling.upload_processor import batch_move_files, SampleGroup
 
 
-def samples_from_csv(bucket_name, prefix):
+def samples_from_csv(bucket_name, prefix) -> Tuple[List[str], str]:
     """Determines list of samples from a csv file within a specified bucket
 
     Assumptions
@@ -44,7 +44,7 @@ def samples_from_csv(bucket_name, prefix):
 
 def determine_samples(
     upload_bucket: str, upload_prefix: str, metadata_bucket: str, metadata_prefix: str
-):
+) -> Tuple[Set[str], str]:
     """Determine files that should be moved to main vs archive bucket.
     Determines the difference between the latest CSV file within upload
     and the CSV file from the most recent batch as the list of samples to
@@ -63,15 +63,54 @@ def determine_samples(
     return samples, curr_csv_file_path
 
 
-def generate_file_list(samples: List[str]):
-    """ Generate list of expected files, given a list of sampleIDs """
-    main_files: List[str] = []
-    archive_files: List[str] = []
+def generate_file_list(
+    samples: Set[str],
+) -> Tuple[List[SampleGroup], List[SampleGroup]]:
+    """ Generate list of expected files, given a list of samples """
+    main_files = []
+    archive_files = []
     for s in samples:
-        main_files.extend([f'{s}.g.vcf.gz', f'{s}.g.vcf.gz.tbi', f'{s}.g.vcf.gz.md5'])
-        archive_files.extend([f'{s}.cram', f'{s}.cram.crai', f'{s}.cram.md5'])
+        sample_group_main = SampleGroup(
+            f'{s}.g.vcf.gz', f'{s}.g.vcf.gz.tbi', f'{s}.g.vcf.gz.md5'
+        )
+        main_files.append(sample_group_main)
+        sample_group_archive = SampleGroup(
+            f'{s}.cram', f'{s}.cram.crai', f'{s}.cram.md5'
+        )
+        archive_files.append(sample_group_archive)
 
     return main_files, archive_files
+
+
+def setup_job(batch: hb.batch, name: str, docker_image: Optional[str]) -> hb.batch.job:
+    """ Returns a new Hail Batch job that activates the Google service account. """
+
+    job = batch.new_job(name=name)
+    if docker_image is not None:
+        job.image(docker_image)
+
+    job.command('gcloud -q auth activate-service-account --key-file=/gsa-key/key.json')
+
+    return job
+
+
+def validate_md5(
+    job: hb.batch.job, sample_group: SampleGroup, upload_path: str
+) -> hb.batch.job:
+    """Each gvcf and cram file is uploaded with a corresponding md5 checksum.
+    This function appends commands to the job that moves these files, to validate each .md5 file."""
+
+    # Generate paths to files that are being validated
+    path_to_data = join('gs://', upload_path, sample_group.data_file)
+    path_to_md5 = join('gs://', upload_path, sample_group.md5)
+
+    # Calculate md5 checksum.
+    job.command(
+        f'gsutil cat {path_to_data} | md5sum | sed "s/-/{sample_group.data_file}/" > /tmp/{sample_group.md5}'
+    )
+    job.command(f'diff /tmp/{sample_group.md5} <(gsutil cat {path_to_md5})')
+
+    return job
 
 
 @click.command()
@@ -118,33 +157,42 @@ def run_processor(
 
     batch = hb.Batch(name='Process files', backend=service_backend)
 
-    # Moving the files to the main bucket
-    main_jobs = batch_move_files(
-        batch,
-        main_files,
-        upload_path,
-        main_path,
-        docker_image,
-        key,
-    )
+    main_jobs = []
+    for sample_group in main_files:
+        # Moving the files to the main bucket
+        sample_group_main_jobs = batch_move_files(
+            batch,
+            sample_group,
+            upload_path,
+            main_path,
+            docker_image,
+            key,
+        )
 
-    # Moving the files to the archive bucket
-    archive_jobs = batch_move_files(
-        batch,
-        archive_files,
-        upload_path,
-        archive_path,
-        docker_image,
-        key,
-    )
+        validate_job = setup_job(batch, 'Validate MD5', docker_image)
+        validate_job = validate_md5(validate_job, sample_group, main_path)
+        validate_job.depends_on(*sample_group_main_jobs)
+        main_jobs.append(validate_job)
+
+    archive_jobs = []
+    for sample_group in archive_files:
+        # Moving the files to the archive bucket
+        sample_group_archive_jobs = batch_move_files(
+            batch,
+            sample_group,
+            upload_path,
+            archive_path,
+            docker_image,
+            key,
+        )
+        validate_job = setup_job(batch, 'Validate MD5', docker_image)
+        validate_job = validate_md5(validate_job, sample_group, archive_path)
+        validate_job.depends_on(*sample_group_archive_jobs)
+        archive_jobs.append(validate_job)
 
     # Move the csv file to the metadata bucket, after the gVCFs and CRAMs have been
     # moved to the main and archive buckets.
-    csv_job = batch.new_job(name=f'Move {basename(csv_path)}')
-    csv_job.image(docker_image)
-    csv_job.command(
-        'gcloud -q auth activate-service-account --key-file=/gsa-key/key.json'
-    )
+    csv_job = setup_job(batch, f'Move {basename(csv_path)}', docker_image)
     csv_job.command(f'gsutil mv gs://{csv_path} gs://{metadata_bucket}/{batch_path}/')
     csv_job.depends_on(*main_jobs)
     csv_job.depends_on(*archive_jobs)
