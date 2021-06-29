e8e3445d70b23d9da47523ed8231815ecfd76158
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-06-30 01:08:52 +1000

    Fix

--- workflows/batch_mt2vcf.py
+++ workflows/batch_mt2vcf.py
@@ -10,18 +10,11 @@ https://github.com/populationgenomics/analysis-runner (see helper script `driver
 """
 
 import os
-import subprocess
 from os.path import join, dirname, abspath
-from typing import List, Optional, Tuple
 import logging
-import click
-import pandas as pd
 import hailtop.batch as hb
-from hailtop.batch.job import Job
 from analysis_runner import dataproc
-
 from joint_calling import utils
-from joint_calling.variant_qc import add_variant_qc_jobs
 
 logger = logging.getLogger('joint-calling')
 logger.setLevel('INFO')
@@ -33,10 +26,10 @@ def main():  # pylint: disable=too-many-arguments,too-many-locals,too-many-state
     either a Hail Query script from the scripts folder in this repo using a Dataproc
     cluster; or a GATK command using the GATK or Gnarly image.
     """
-    logger.info(f'Enable random forest: {run_rf}')
-    logger.info(f'Enable VQSR: {run_vqsr}')
+    logger.info(f'Enable random forest')
+    logger.info(f'Enable VQSR')
 
-    billing_project = os.getenv('HAIL_BILLING_PROJECT') or callset_name
+    billing_project = os.getenv('HAIL_BILLING_PROJECT')
     hail_bucket = os.environ.get('HAIL_BUCKET')
     logger.info(
         f'Starting hail Batch with the project {billing_project}, '
@@ -53,14 +46,14 @@ def main():  # pylint: disable=too-many-arguments,too-many-locals,too-many-state
     scripts_dir = abspath(join(dirname(__file__), '..', 'scripts'))
 
     mt_path = 'gs://cpg-tob-wgs-test/mt/v2-raw.mt'
-    meat_ht_path = 'gs://cpg-tob-wgs-main-metadata/joint-calling/v2/meta.ht/'
+    meta_ht_path = 'gs://cpg-tob-wgs-main-metadata/joint-calling/v2/meta.ht/'
     hard_filter_ht_path = (
         'gs://cpg-tob-wgs-test-tmp/joint-calling/v3/sample_qc/hard_filters.ht/'
     )
     combined_vcf_path = (
         'gs://cpg-tob-wgs-test-tmp/joint-calling/v2/variant_qc/vqsr/input-test.vcf.gz'
     )
-    mt_to_vcf_job = dataproc.hail_dataproc_job(
+    dataproc.hail_dataproc_job(
         b,
         f'{scripts_dir}/mt_to_vcf.py --overwrite '
         f'--mt {mt_path} '
@@ -69,8 +62,7 @@ def main():  # pylint: disable=too-many-arguments,too-many-locals,too-many-state
         f'-o {combined_vcf_path} ',
         max_age='8h',
         packages=utils.DATAPROC_PACKAGES,
-        num_secondary_workers=scatter_count,
-        depends_on=depends_on,
+        num_secondary_workers=50,
         secondary_worker_boot_disk_size=10,
         job_name='MT to VCF',
     )
