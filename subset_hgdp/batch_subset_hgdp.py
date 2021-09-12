e2fbd65d1325f2e0fa6ee4adc7185e697a986da3
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-09-12 21:10:43 +1000

    Add subset_hgdp

---
+++ subset_hgdp/batch_subset_hgdp.py
@@ -0,0 +1,59 @@
+"""
+Submit a hail query script that pre-subsets the HGDP matrix table
+"""
+
+import os
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+
+POP = 'nfe'
+
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'),
+    bucket=os.getenv('HAIL_BUCKET'),
+)
+batch = hb.Batch(name='subset-gnomad-hgdp-1kg', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    'subset_hgdp.py',
+    max_age='12h',
+    packages=['selenium'],
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    job_name='subset-gnomad-hgdp-1kg',
+    num_secondary_workers=50,
+)
+
+dataproc.hail_dataproc_job(
+    batch,
+    'subset_hgdp.py --test',
+    max_age='12h',
+    packages=['selenium'],
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    job_name='subset-gnomad-hgdp-1kg',
+    num_secondary_workers=50,
+)
+
+dataproc.hail_dataproc_job(
+    batch,
+    'subset_hgdp.py --pop',
+    max_age='12h',
+    packages=['selenium'],
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    job_name='subset-gnomad-hgdp-1kg',
+    num_secondary_workers=50,
+)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'subset_hgdp.py --test --pop {POP}',
+    max_age='12h',
+    packages=['selenium'],
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    job_name='subset-gnomad-hgdp-1kg',
+    num_secondary_workers=50,
+)
+
+batch.run()
---
+++ subset_hgdp/subset_hgdp.py
@@ -0,0 +1,104 @@
+"""
+QC of newly-selected variants
+"""
+
+import os
+from typing import Optional
+import click
+import hail as hl
+
+
+GNOMAD_HGDP_1KG_MT_PATH = (
+    'gs://gcp-public-data--gnomad/release/3.1/mt/genomes/'
+    'gnomad.genomes.v3.1.hgdp_1kg_subset_dense.mt'
+)
+
+REF_BUCKET = 'gs://cpg-reference/hg38/v1'
+
+OUTPUT_PATH = os.path.join(
+    REF_BUCKET, 'mt/gnomad.genomes.v3.1.hgdp_1kg_subset_dense{suf}.mt'
+)
+
+
+@click.command()
+@click.option('--test', 'test', is_flag=True)
+@click.option('--pop', 'pop')
+def main(
+    test: bool,
+    pop: Optional[str],
+):  # pylint: disable=missing-function-docstring
+    hl.init(default_reference='GRCh38')
+    mt = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT_PATH)
+    suf = ''
+
+    if test:
+        suf = '_test'
+        # Subset to 50 samples
+        ncols = mt.count_cols()
+        target_ncols = 50
+        mt = mt.sample_cols(p=target_ncols / ncols, seed=42)
+        mt.write(OUTPUT_PATH.format(suf=suf), overwrite=True)
+        mt = hl.read_matrix_table(OUTPUT_PATH.format(suf=suf))
+
+    if pop:
+        suf = f'{suf}_{pop}'
+        # Get samples from the specified population only
+        mt = mt.filter_cols(
+            ~hl.is_defined(mt.hgdp_1kg_metadata)
+            | (mt.hgdp_1kg_metadata.population_inference.pop == pop.lower())
+        )
+        mt.write(OUTPUT_PATH.format(suf=suf), overwrite=True)
+        mt = hl.read_matrix_table(OUTPUT_PATH.format(suf=suf))
+
+    filter_high_quality_sites(
+        mt,
+        out_mt_path=OUTPUT_PATH.format(suf=f'{suf}_hq'),
+    )
+
+
+def filter_high_quality_sites(
+    mt: hl.MatrixTable,
+    num_rows_before_ld_prune: int = 200_000,
+    out_mt_path: str = None,
+) -> hl.MatrixTable:
+    """
+    1. Run `hl.variant_qc()` to calculate metrics such as AF, call rate
+        and inbereeding coefficient
+    2. Select variants based off of gnomAD v3 criteria: AF > 1%, call rate > 99%,
+        inbreeding coefficient >-0.25 (no excess of heterozygotes)
+    3. Randomly subset sites to `num_rows_before_ld_prune`
+    4. LD-prune
+    """
+    print(f'Number of rows before filtering: {mt.count_rows()}')
+
+    # Choose variants based off of gnomAD v3 criteria
+    mt = hl.variant_qc(mt)
+    mt = mt.annotate_rows(IB=hl.agg.inbreeding(mt.GT, mt.variant_qc.AF[1]))
+    mt = mt.filter_rows(
+        (mt.variant_qc.AF[1] > 0.01)
+        & (mt.variant_qc.call_rate > 0.99)
+        & (mt.IB.f_stat > -0.25)
+    )
+
+    # Randomly subsampling the matrix table to `num_rows_before_ld_prune` sites
+    # before feeding it into LD prunning
+    mt = mt.cache()
+    nrows = mt.count_rows()
+    print(f'Number of rows after filtering: {nrows}')
+    if nrows > num_rows_before_ld_prune:
+        print(f'Number of rows {nrows} > {num_rows_before_ld_prune}, subsetting')
+        mt = mt.sample_rows(num_rows_before_ld_prune / nrows, seed=12345)
+
+    # LD prunning
+    pruned_variant_ht = hl.ld_prune(mt.GT, r2=0.1, bp_window_size=500000)
+    mt = mt.filter_rows(hl.is_defined(pruned_variant_ht[mt.row_key]))
+    print(f'Number of rows after prunning: {mt.count_rows()}')
+
+    if out_mt_path:
+        mt.write(out_mt_path, overwrite=True)
+        mt = hl.read_matrix_table(out_mt_path)
+    return mt
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=E1120
